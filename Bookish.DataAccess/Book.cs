48385d11fca3250c6c4ba706194de1228005b7a7
wilson-zhang21 <wilson.zhang21@outlook.com>
2021-11-24 16:31:44 +0000

    Part 2 Done

---
+++ Bookish.DataAccess/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookish.DataAccess
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+    }
+}
---
+++ Bookish.DataAccess/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Bookish.DataAccess
+{
+    public class Program
+    {
+        private IDbConnection db = new SqlConnection("Server=localhost;Database=Bookish;Trusted_Connection=True");
+
+    }
+}
